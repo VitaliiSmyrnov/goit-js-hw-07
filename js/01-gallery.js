3ae7291c6ae250832c70d7d46545ce9a24dc4e34
VitaliiSmyrnov <vitaliy.smirnov@ukr.net>
2022-11-03 17:33:38 +0200

    task 07  1-1 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainer = document.querySelector('.gallery');
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<div class="gallery__item">
+    <a class="gallery__link" href="large-image.jpg">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`
+  )
+  .join('');
+
+galleryContainer.insertAdjacentHTML('beforeend', markup);
 
 console.log(galleryItems);
